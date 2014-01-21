e71330102de20ec65e5f6e961c24a5a872a1f6f2
adamsong <adamson.g@gmail.com>
2014-01-21 09:49:00 -0500

    Started working on localizations

---
+++ src/thecyberplanners/labyrinthine/Main.java
@@ -0,0 +1,22 @@
+package thecyberplanners.labyrinthine;
+
+import com.jme3.app.SimpleApplication;
+
+public class Main extends SimpleApplication {
+	
+	@Override
+	public void simpleInitApp() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Main game = new Main();
+		game.start();
+		
+	}
+	
+}
---
+++ src/thecyberplanners/labyrinthine/localizatoin/LocalizationLoader.java
@@ -0,0 +1,5 @@
+package thecyberplanners.labyrinthine.localizatoin;
+
+public class LocalizationLoader {
+	
+}
