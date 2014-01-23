07054fa7957ce959a80ffed77261d45cf197a819
adamsong <adamson.g@gmail.com>
2014-01-23 06:35:30 -0500

    Render distance now in chunks

--- src/thecyberplanners/labyrinthine/MainComponent.java
+++ src/thecyberplanners/labyrinthine/MainComponent.java
@@ -5,14 +5,18 @@ import java.util.Random;
 import com.jme3.app.SimpleApplication;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
+import com.jme3.niftygui.NiftyJmeDisplay;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Spatial;
 import com.jme3.scene.shape.Box;
 import com.jme3.system.AppSettings;
 
+import de.lessvoid.nifty.Nifty;
+
 public class MainComponent extends SimpleApplication {
 	
-	public static final int RENDER_SIZE = 10;
+	public static final int RENDER_DISTANCE = 10;
 	public static final int SCALE = 2;
 	public static final double ROOM_FREQUENCY = 0.0025;
 	
@@ -24,6 +28,8 @@ public class MainComponent extends SimpleApplication {
 		MainComponent app = new MainComponent();
 		app.setSettings(settings);
 		app.setShowSettings(false);
+		app.setDisplayStatView(false);
+		app.setDisplayFps(false);
 		app.start();
 	}
 	
@@ -41,10 +47,10 @@ public class MainComponent extends SimpleApplication {
 		rootNode.attachChild(createBox("Room 8", SCALE / 2, SCALE / 2, SCALE / 2, ColorRGBA.Orange).move(-2f, 0, -2f));
 		
 		Random random = new Random();
-		for (int x = -RENDER_SIZE; x <= RENDER_SIZE; x++) {
-			for (int z = -RENDER_SIZE; z <= RENDER_SIZE; z++) {
+		for (int x = -RENDER_DISTANCE*16; x <= 16*RENDER_DISTANCE; x++) {
+			for (int z = -RENDER_DISTANCE*16; z <= 16*RENDER_DISTANCE; z++) {
 				if (ENABLE_3D_GEN) {
-					for (int y = -RENDER_SIZE; y <= RENDER_SIZE; y++) {
+					for (int y = -RENDER_DISTANCE*16; y <= 16*RENDER_DISTANCE; y++) {
 						if ((Math.abs(random.nextDouble() % 100)) < ROOM_FREQUENCY && !((x >= -1 && x <= 1) && (z <= 1 && z >= -1))) {
 							int num = Math.abs(random.nextInt() % 3);
 							switch (num) {
@@ -63,7 +69,7 @@ public class MainComponent extends SimpleApplication {
 							}
 						}
 					}
-				}else{
+				} else {
 					if ((Math.abs(random.nextDouble() % 100)) < ROOM_FREQUENCY && !((x >= -1 && x <= 1) && (z <= 1 && z >= -1))) {
 						int num = Math.abs(random.nextInt() % 3);
 						switch (num) {
@@ -84,6 +90,14 @@ public class MainComponent extends SimpleApplication {
 				}
 			}
 		}
+		
+		NiftyJmeDisplay niftyDisplay = new NiftyJmeDisplay(assetManager, inputManager, audioRenderer, guiViewPort);
+		Nifty nifty = niftyDisplay.getNifty();
+		MainMenu ss = new MainMenu(this);
+		stateManager.attach(ss);
+		nifty.fromXml("Interfaces/screen.xml", "start", ss);
+		guiViewPort.addProcessor(niftyDisplay);
+		flyCam.setDragToRotate(true);
 	}
 	
 	private Spatial createBox(String string, int i, int j, int k, ColorRGBA color) {
@@ -92,5 +106,10 @@ public class MainComponent extends SimpleApplication {
 		material.setColor("Color", color);
 		box.setMaterial(material);
 		return box;
+	}
+	@Override
+	public void simpleUpdate(float tpf) {
+		Vector3f v = cam.getLocation();
+		System.out.println("Camera is at " + v.toString());
 	}
 }
