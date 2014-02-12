797c8e6161402a9ad8cba518739e2f1a0f4b3c62
adamsong <adamson.g@gmail.com>
2014-02-12 18:32:00 -0500

    Built for most platforms, added basic functionality

---
+++ unity/Assets/Scripts/cycleCams.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cycleCams : MonoBehaviour {
+
+	public Camera[] cameras;
+	public int cam;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("init");
+		for (int i = 1; i<4; i++) {
+			cameras[i].gameObject.SetActive(false);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.F)){
+			print("Switching camera");
+			cameras[cam].gameObject.SetActive(false);
+			cam++;
+			if(cam == 4)
+				cam = 0;
+			cameras[cam].gameObject.SetActive(true);
+		}
+	}
+}
